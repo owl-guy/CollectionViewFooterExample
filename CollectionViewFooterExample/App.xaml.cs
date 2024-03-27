e08327eeced1e2848206653046375e72fe66d379
Euler Urs <Euler.Urs@kermi.de>
2024-03-27 09:20:07 +0100

    Add project files.

---
+++ CollectionViewFooterExample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionViewFooterExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CollectionViewFooterExample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewFooterExample
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<string> _items;
+        public ObservableCollection<string> Items
+        {
+            get => _items;
+            set {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Random _random;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            _random = new Random();
+            Items = new ObservableCollection<string>();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Items.Add(_random.Next().ToString());
+        }
+    }
+
+}
